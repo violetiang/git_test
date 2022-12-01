31577fc43cf747e601a4ddce505386e39885f565
Violet <Violet@gmail.com>
2022-12-01 23:10:20 +0800

    hot-fix commit1

--- src/main/java/com/qj/GitTest.java
+++ src/main/java/com/qj/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello");
         System.out.println("Hey");
+        System.out.println("Welcome My Git");
     }
 }
